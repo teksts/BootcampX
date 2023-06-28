01752bee4f9a1bc67e75df67e370b31b91a0bf8f
teksts <mark.egelhoff1@gmail.com>
2023-06-28 14:46:58 -0700

    Implement node-postgres app

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -9,11 +9,13 @@ const pool = new Pool({
 });
 
 pool.query(`
-SELECT students.id as student_id, students.name as student, cohorts.name as cohort
-FROM students
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM assistance_requests
+JOIN teachers ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
 WHERE cohorts.name LIKE '%${args[0]}%'
-LIMIT ${args[1]};
+ORDER BY teacher;
 `)
   .then(res => {
     console.log(res.rows);
