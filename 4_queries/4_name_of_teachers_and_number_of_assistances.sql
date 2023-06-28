333913a78579dcf38db4ca97bb0b9fb7bf751cce
teksts <mark.egelhoff1@gmail.com>
2023-06-28 14:34:25 -0700

    Set up the JS project

--- 4_queries/4_name_of_teachers_and_number_of_assistances.sql
+++ 4_queries/4_name_of_teachers_and_number_of_assistances.sql
@@ -3,7 +3,6 @@ FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = 'JUL02'
+-- WHERE cohorts.name = 'JUL02'
 GROUP BY teachers.name, cohorts.name
 ORDER BY teacher;
-
